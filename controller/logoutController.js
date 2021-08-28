a88f37a20e1f5a1d6bcd3da885f280e065e4ff54
Rahul Behera <rahulbehera2002@gmail.com>
2021-08-28 17:46:51 +0530

    Added Logout Route

---
+++ controller/logoutController.js
@@ -0,0 +1,4 @@
+module.exports.logout = function(req,res){
+    console.log(req.cookie);
+    res.cookie('jwt','',{expires:0});
+}
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,5 @@ const router = express.Router();
 router.use('/student',require('./studentSignUpRoute'));
 router.use('/student',require('./studentLoginRoute'));
 router.use('/teacher',require('./teacherSignUpRoute'));
+router.use('/logout', require('./logoutRoute'));
 module.exports = router;
