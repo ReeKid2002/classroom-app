0c20a573487238edd5ef643fa3aee367aad875c2
Rajveer Kumar <rk2279709@gmail.com>
2021-08-29 03:49:21 +0530

    Manual Login

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 router.get('/',function(request,reposne){
     return reposne.render('landingPage');
-})
+});
 router.use('/student',require('./studentSignUpRoute'));
 router.use('/student',require('./studentLoginRoute'));
 router.use('/teacher',require('./teacherSignUpRoute'));
